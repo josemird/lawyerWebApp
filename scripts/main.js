dcdc9801879bf7dc1a22d6a8efe457e6813cf1de
José Miguel <josemidaw22@gmail.com>
2024-02-24 00:02:41 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -24,3 +24,19 @@ button.addEventListener('click', () => {
             }
         });
 });
+
+async function cargarMapaAsync() {
+    await new Promise(resolve => {
+        document.addEventListener("DOMContentLoaded", resolve);
+    });
+
+    let mapContainer = document.querySelector(".secMapa");
+    let mapIframe = document.createElement('iframe');
+    mapIframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3200.5068500595403!2d-4.567662822293242!3d36.662312775457174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72e576912d1a6f%3A0xa3c56d8f4ca9f807!2sMartin%20Marin%20abgados!5e0!3m2!1ses!2ses!4v1708726537659!5m2!1ses!2ses";
+    mapIframe.allowfullscreen = "";
+    mapIframe.loading = "lazy";  
+    mapIframe.referrerpolicy = "no-referrer-when-downgrade";
+    mapContainer.appendChild(mapIframe);
+}
+
+cargarMapaAsync();
